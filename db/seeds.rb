44b70b1b42653587afe153e33ea3ec578adb3180
Benjamin Jackson <benj@betterrentals.com.au>
2024-05-29 12:10:26 +1000

    le wagon

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,49 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
 puts "Clearing out your junk.."
+List.destroy_all
+Bookmark.destroy_all
 Movie.destroy_all
 
+movies = []
+lists = []
+
+uri = "https://tmdb.lewagon.com/movie/top_rated"
+html_file = URI.open(uri).read
+srlzd = JSON.parse(html_file)
+
+descs = ["Laughs", "Feels", "Really Feels", "Bang Bang", "Alex Lives Here", "Bruce Willis?", "Science is Golden", "Just Tolkien", "Tuesday Nights", "Weird Shit", "For Dog", "Wes Anderson 4EVA"]
+
+puts "Creating Lists.."
+12.times do |i|
+  list = List.create!(name: "#{descs[i]}")
+  lists << list
+  puts "##{i} done"
+end
+puts "Lists done!"
+
 puts "Creating movies.."
-10.times do |i|
-  Movie.create!(title: Faker::Movie.title, content: Faker::Movie.quote)
+18.times do |i|
+  result = srlzd["results"][i]
+  title = result["title"]
+  overview = result["overview"]
+  poster = result["poster_path"]
+  rating = result["vote_average"]
+  poster_url = "https://image.tmdb.org/t/p/w500#{poster}"
+  movie = Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  movies << movie
   puts "##{i} done"
 end
 puts "All movies added!"
+
+puts "Creating bookmarks.."
+10.times do |i|
+  comment = Faker::Fantasy::Tolkien.poem
+  Bookmark.create!(comment: comment, movie: movies.sample, list: lists.sample)
+  puts "##{i} done"
+end
+puts "All bookmarks added!"
